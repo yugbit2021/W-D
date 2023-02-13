eb6b45ab434a19a21fd610eed96864713a4441e6
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-13 10:10:16 +0530

    L-2

---
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,6 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        alert("I got clicked");
+        });
+}
+
