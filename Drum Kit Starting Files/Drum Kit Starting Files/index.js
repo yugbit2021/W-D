2f1efc6d94d7cd5008ceabec3a2767995b36addf
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-13 11:18:32 +0530

    l-4

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,37 +1,45 @@
 
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll("button")[i].addEventListener("click", function(){
-      var buttonInnerHtml = this.innerHTML;
-switch (buttonInnerHtml) {
-    case "w":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-        break;
-    case "a":
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-        break;
-    case "s":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-        break;
-    case "d":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-    case "j":
-      var audio = new Audio("sounds/tom-2.mp3");
-     audio.play();
-     break;
-    case "l":
-         var audio = new Audio("sounds/tom-3.mp3");
+      var buttonInnerHtml =this.innerHTML;
+      makesound(buttonInnerHtml);
+});
+}
+
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+});
+
+function makesound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+          var audio = new Audio("sounds/tom-2.mp3");
          audio.play();
          break;
-
-    default:
-      var audio = new Audio("sounds/tom-4.mp3");
-      audio.play();
-        
-}
-});
+        case "l":
+             var audio = new Audio("sounds/tom-3.mp3");
+             audio.play();
+             break;
+    
+        default:
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+            
+    }
 }
