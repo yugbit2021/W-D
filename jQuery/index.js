d1ffe9be789cee31823dd57621b5162642f20241
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-14 15:39:59 +0530

    Adding elements using jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,4 @@
-$("h1").on("mouseover", function(event){
-  $("h1").css("color","purple");
-});
+$("h1").before("<button>New</button");
+$("h1").after("<button>New</button");
+$("h1").prepend("<button>YHH</button");
+$("h1").append("<button>hhy</button");
