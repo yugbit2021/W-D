a2976fb927e40ee93a2df175ecf947f1e3b445a5
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-16 22:15:02 +0530

    creating our first server with express

--- into_to_node/index.js
+++
@@ -1,7 +0,0 @@
-const supervillains = require('supervillains');
-
-supervillains.all;
-//=> ['Abattoir', 'Able Crown', …]
-
-const mysupervillains=supervillains.random();
-console.log(mysupervillains);
