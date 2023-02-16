727c5f61ebb0ffd315ec902d92f35e4c27204f68
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-16 22:30:32 +0530

    Handling Requests and Responses

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/",function(request,response){
+    response.send("<h1>Hello , World</h1>");
+});
+
 app.listen(3000,function(){
     console.log("server started at port 3000");
 });
