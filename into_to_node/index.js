1222712210eb703785a16d948263c3c97650cb2f
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-16 19:09:53 +0530

    Native node modulus

---
+++ into_to_node/index.js
@@ -0,0 +1,4 @@
+//jshint esversion:6
+
+const fs= require("fs");
+fs.copyFileSync("file.txt","file1.txt");
--- jQuery/index.js
+++ jQuery/index.js
@@ -1,16 +1,22 @@
 // $("button").on("click",function(){
 //     $("h1").hide();
 // });
+
+
 // $("button").on("click",function(){
 //     $("h1").fadeOut();
 // });
 
+
 // $("button").on("click",function(){
 //     $("h1").fadeToggle();
 // });
+
+
 // $("button").on("click",function(){
 //     $("h1").slideUp();
 // });
+
 
 $("button").on("click",function(){
     $("h1").animate({opacity:0.5});
