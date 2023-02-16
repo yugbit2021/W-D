fbc912f778cb249dabfd5e84aed05c6753d59300
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-16 22:37:54 +0530

    Understanding and working with routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,14 @@ app.get("/",function(request,response){
     response.send("<h1>Hello , World</h1>");
 });
 
+app.get("/contact",function(req,res){
+    res.send("Contact at jugeshsm@gmail.com");
+});
+
+app.get("/about",function(req,res){
+    res.send("Hello my name is Jugesh Kumar");
+});
+
 app.listen(3000,function(){
     console.log("server started at port 3000");
 });
